92789870ea63f5fa16e653907098d7072d4a13f4
sangwoon <sangwoon.kim@epapyrus.com>
2025-03-01 17:15:24 +0900

    git init

---
+++ src/main/java/com/sangwoon/kim/oodp/OodpApplication.java
@@ -0,0 +1,13 @@
+package com.sangwoon.kim.oodp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OodpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OodpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangwoon/kim/oodp/OodpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangwoon.kim.oodp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OodpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
